7635a045f32666b37b024553b32358bc26178820
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-03-14 18:48:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,25 +14,27 @@ const Enmap = require("enmap"); //this package is our Database! We will use it t
 const fs = require("fs"); //this package is for reading files and getting their inputs
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
 const client = new Discord.Client({
-//  messageCacheLifetime: 60,
   fetchAllMembers: false,
-//  messageCacheMaxSize: 10,
   restTimeOffset: 0,
+  shards: "auto",
   restWsBridgetimeout: 100,
   disableEveryone: true,
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
+require('events').EventEmitter.defaultMaxListeners = 100;
+process.setMaxListeners(100);
 //Loading files, with the client variable like Command Handler, Event Handler, ...
 ["clientvariables", "command", "events", "erelahandler", "requestreacts"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
 //Each Database gets a own file and folder which is pretty handy!
-client.premium = new Enmap({name: "premium", dataDir: "./databases/premium", ensureProps: false})
-client.stats = new Enmap({name: "stats", dataDir: "./databases/stats", ensureProps: false})
-client.settings = new Enmap({name: "setups", dataDir: "./databases/settings", ensureProps: false})
-client.setups = new Enmap({name: "setups", dataDir: "./databases/setups", ensureProps: false})
-client.modActions = new Enmap({name: 'actions'});
-client.userProfiles = new Enmap({name: 'userProfiles' });
+client.premium = new Enmap({name: "premium", dataDir: "./databases/premium"})
+client.stats = new Enmap({name: "stats", dataDir: "./databases/stats"})
+client.settings = new Enmap({name: "setups", dataDir: "./databases/settings"})
+client.setups = new Enmap({name: "setups", dataDir: "./databases/setups",})
+client.queuesaves = new Enmap({name: "queuesaves", dataDir: "./databases/queuesaves", ensureProps: false})
+client.modActions = new Enmap({name: 'actions', dataDir: "./databases/warns"});
+client.userProfiles = new Enmap({name: 'userProfiles', dataDir: "./databases/warns" });
 
 //login into the bot
 client.login(require("./botconfig/config.json").token);
