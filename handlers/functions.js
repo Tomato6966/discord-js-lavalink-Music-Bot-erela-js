fdd42f0e8713d6e59051a9bdeacdd96701f82448
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-03-31 23:36:30 +0200

    Add files via upload

--- handlers/functions.js
+++ handlers/functions.js
@@ -487,8 +487,6 @@ module.exports = {
       }
 
       function createBar(total, current, size = 25, line = "▬", slider = "🔶") {
-        let current = player.queue.current.duration !== 0 ? player.position : player.queue.current.duration;
-        let total = player.queue.current.duration;
         if (!player.queue.current) return `**[${slider}${line.repeat(size - 1)}]**\n**00:00:00 / 00:00:00**`;
         let bar = current > total ? [line.repeat(size / 2 * 2), (current / total) * 100] : [line.repeat(Math.round(size / 2 * (current / total))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (current / total)) + 1), current / total];
         if (!String(bar).includes(slider)) return `**[${slider}${line.repeat(size - 1)}]**\n**00:00:00 / 00:00:00**`;
@@ -641,8 +639,6 @@ module.exports = {
       }
 
       function createBar(total, current, size = 25, line = "▬", slider = "🔶") {
-        let current = player.queue.current.duration !== 0 ? player.position : player.queue.current.duration;
-        let total = player.queue.current.duration;
         if (!player.queue.current) return `**[${slider}${line.repeat(size - 1)}]**\n**00:00:00 / 00:00:00**`;
         let bar = current > total ? [line.repeat(size / 2 * 2), (current / total) * 100] : [line.repeat(Math.round(size / 2 * (current / total))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (current / total)) + 1), current / total];
         if (!String(bar).includes(slider)) return `**[${slider}${line.repeat(size - 1)}]**\n**00:00:00 / 00:00:00**`;
