5f8b52b29fb5ac36f131904e455f4b6b0bd47fb1
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-04-01 15:52:37 +0200

    Add files via upload

--- commands/💪 Source-Help/docs.js
+++ commands/💪 Source-Help/docs.js
@@ -12,22 +12,12 @@ module.exports = {
   usage: "docs",
   description: "Shows information about docs",
   run: async (client, message, args, user, text, prefix) => {
-    try {
-      message.channel.send(new MessageEmbed()
-        .setTitle(`This Bot is made by \`Tomato#6966\` and **this** is the Source Code link to this Bot`)
-        .setColor(ee.color)
-        .setFooter(ee.footertext, ee.footericon)
-        .setDescription(`WOAH THERE ARE DOCS!?!!?\n[\`CLICK HERE\`](https://discord.js.org/#/docs/main/stable/general/welcome)\nYes, discord.js has docs that can be used when creating a bot with it.\nIf you need some pointers for using the docs watch a video by one of our Lower Management Members\n\nhttps://www.youtube.com/watch?v=CvcDpm4PsnI`)
-      );
-    } catch (e) {
-      console.log(String(e.stack).bgRed)
-      return message.channel.send(new MessageEmbed()
-        .setColor(ee.wrongcolor)
-        .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`${emoji.msg.ERROR}  ERROR | An error occurred`)
-        .setDescription(`\`\`\`${e.message}\`\`\``)
-      );
-    }
+    message.channel.send(new MessageEmbed()
+      .setTitle(`This Bot is made by \`Tomato#6966\` and **this** is the Source Code link to this Bot`)
+      .setColor(ee.color)
+      .setFooter(ee.footertext, ee.footericon)
+      .setDescription(`WOAH THERE ARE DOCS!?!!?\n[\`CLICK HERE\`](https://discord.js.org/#/docs/main/stable/general/welcome)\nYes, discord.js has docs that can be used when creating a bot with it.\nIf you need some pointers for using the docs watch a video by one of our Lower Management Members\n\nhttps://www.youtube.com/watch?v=CvcDpm4PsnI`)
+    );
   }
 };
 /**
--- commands/💪 Source-Help/source.js
+++ commands/💪 Source-Help/source.js
@@ -12,26 +12,16 @@ module.exports = {
   usage: "sourcecode",
   description: "Shows you the Github and Source Code Information about this Bot",
   run: async (client, message, args, user, text, prefix) => {
-    try {
-      message.channel.send(new MessageEmbed()
-        .setTitle(`This Bot is made by \`Tomato#6966\` and **this** is the Source Code link to this Bot`)
-        .setURL("https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js/")
-        .addField("Discord.js: ", "[\`v12.5.1\`](https://discord.js.org)", true)
-        .addField("Erela.js: ", "[\`v2.3.2\`](https://solaris.codes/projects/erelajs/)", true)
-        .addField("Node.js: ", "[\`v15.3.4\`](https://nodejs.org/en/)", true)
-        .addField("Lavalink: ", "[\`v3.3.2.3\`](https://github.com/Frederikam/Lavalink/releases/tag/3.3.2.3)")
-        .addField("Source Code. ", "Don't just use the source for yourself,... please [invite](https://discord.com/api/oauth2/authorize?client_id=742672021422342165&permissions=8&scope=bot) me too![\`Click here\`](https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js/)")
-        .setColor(ee.color).setFooter(ee.footertext, ee.footericon)
-      );
-    } catch (e) {
-      console.log(String(e.stack).bgRed)
-      return message.channel.send(new MessageEmbed()
-        .setColor(ee.wrongcolor)
-        .setFooter(ee.footertext, ee.footericon)
-        .setTitle(`${emoji.msg.ERROR}  ERROR | An error occurred`)
-        .setDescription(`\`\`\`${e.message}\`\`\``)
-      );
-    }
+    message.channel.send(new MessageEmbed()
+      .setTitle(`This Bot is made by \`Tomato#6966\` and **this** is the Source Code link to this Bot`)
+      .setURL("https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js/")
+      .addField("Discord.js: ", "[\`v12.5.1\`](https://discord.js.org)", true)
+      .addField("Erela.js: ", "[\`v2.3.2\`](https://solaris.codes/projects/erelajs/)", true)
+      .addField("Node.js: ", "[\`v15.3.4\`](https://nodejs.org/en/)", true)
+      .addField("Lavalink: ", "[\`@dev\`](https://cdn.discordapp.com/attachments/798196676405755905/827174915714711572/Lavalink.jar)")
+      .addField("Source Code. ", "Don't just use the source for yourself,... please [invite](https://discord.com/api/oauth2/authorize?client_id=742672021422342165&permissions=8&scope=bot) me too![\`Click here\`](https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js/)")
+      .setColor(ee.color).setFooter(ee.footertext, ee.footericon)
+    );
   }
 };
 /**
