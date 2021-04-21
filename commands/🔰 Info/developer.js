66d332448164a2620c673415b54798a1c65e1e16
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-04-21 21:32:49 +0200

    Add files via upload

--- commands/🔰 Info/developer.js
+++ commands/🔰 Info/developer.js
@@ -16,19 +16,19 @@ module.exports = {
 				.setColor(ee.color)
 				.setFooter(ee.footertext + " | Sponsor: Bittmax.de | Code  'x10'  == -5%", ee.footericon)
 				.setTimestamp()
-				.setThumbnail("https://cdn.discordapp.com/avatars/442355791412854784/17ccf170d62e5e0040660d003b075528.webp")
+				.setThumbnail("https://cdn.discordapp.com/avatars/442355791412854784/df7b527a701d9a1ab6d73213576fe295.webp?size=1024")
 				.setTitle("Tomato#6966 | Dev of 3 Orgs | Developer of 200+ BOTS")
-				.setURL("https://musicium.eu")
+				.setURL("https://milrato.eu")
 				.setDescription(`
-			> Hello I am **Tomato** <@442355791412854784> *(\`Tomato#6966\`)*, and i made more then 200 different Discord Bots: [My Bot List](https://bots.musicium.eu)
+> Hello I am **Tomato** <@442355791412854784> *(\`Tomato#6966\`)*, and i made more then 200 different Discord Bots: [My Bot List](https://bots.musicium.eu)
 
-			> I am very proud for all of my verified and not verified Discord Bots, but the Bot I am the most proud of is: [**Musicium** | \`Ultimate Music experience\`](https://musicium.eu) <@769642999227351070>
+> I am very proud for all of my verified and not verified Discord Bots, but the Bot I am the most proud of is: [**LAVA MUSIC** | \`2021\'s Best Music Bot\`](https://lava.milrato.eu) <@742672021422342165>
 
-			> I made this Bot, and you can get a free Bot too! Just go to: [My Service](https://milrato)
+> I made this Bot, and you can get a free Bot too! Just go to: [My Service](https://milrato)
 
-			> I am also a **Website** Developer and **Plugin** dev. I made **Modules** like a **SPEECH Module** [SEE IT](https://cdn.discordapp.com/attachments/778714123785076768/801606374105546782/2021-01-21_01-15-50.mp4)
+> I am also a **Website** Developer and **Plugin** dev. I made **Modules** like a **SPEECH Module** [SEE IT](https://cdn.discordapp.com/attachments/778714123785076768/801606374105546782/2021-01-21_01-15-50.mp4)
 
-			> Yeah i hope you like my stuff :v: <3`)
+> Yeah i hope you like my stuff :v: <3`)
 			).catch(error => console.log(error));
 		} catch (e) {
 			console.log(String(e.stack).bgRed)
