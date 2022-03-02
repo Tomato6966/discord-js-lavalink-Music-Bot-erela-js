46e2adffad169f70c7d51978f40730213c79e396
Almazmsi <72695998+Almazmsi@users.noreply.github.com>
2022-03-02 13:27:22 +0200

    Fixed playprevious.js

--- commands/🎶 Music/playprevious.js
+++ commands/🎶 Music/playprevious.js
@@ -27,7 +27,7 @@ module.exports = {
         embeds: [
           new MessageEmbed().setColor(es.color)
           .setTitle(`${emoji.msg.search} Playing the Previous Track on ${emoji.msg.soundcloud} Soundcloud`)
-          .setDescription(`[${layer.queue.previous.title}](${layer.queue.previous.uri})`)
+          .setDescription(`[${layer.queue.previous.title}](${player.queue.previous.uri})`)
         ]
       })
       playermanager(client, message, Array(player.queue.previous.uri), `song:soundcloud`);
@@ -36,7 +36,7 @@ module.exports = {
         embeds: [
           new MessageEmbed().setColor(es.color)
           .setTitle(`${emoji.msg.search} Playing the Previous Track on ${emoji.msg.spotify} Spotify`)
-          .setDescription(`[${layer.queue.previous.title}](${layer.queue.previous.uri})`)
+          .setDescription(`[${layer.queue.previous.title}](${player.queue.previous.uri})`)
         ]
       })
       playermanager(client, message, Array(player.queue.previous.uri), `song:raw`);
@@ -45,7 +45,7 @@ module.exports = {
         embeds: [
           new MessageEmbed().setColor(es.color)
           .setTitle(`${emoji.msg.search} Playing the Previous Track on ${emoji.msg.apple} Apple-Music`)
-          .setDescription(`[${layer.queue.previous.title}](${layer.queue.previous.uri})`)
+          .setDescription(`[${layer.queue.previous.title}](${player.queue.previous.uri})`)
         ]
       })
       playermanager(client, message, Array(player.queue.previous.uri), `song:raw`);
@@ -54,7 +54,7 @@ module.exports = {
         embeds: [
           new MessageEmbed().setColor(es.color)
           .setTitle(`${emoji.msg.search} Playing the Previous Track on ${emoji.msg.youtube} Youtube`)
-          .setDescription(`[${layer.queue.previous.title}](${layer.queue.previous.uri})`)
+          .setDescription(`[${layer.queue.previous.title}](${player.queue.previous.uri})`)
         ]
       })
       //play from YOUTUBE
@@ -70,4 +70,5 @@ module.exports = {
  * @INFO
  * Please mention Him / Milrato Development, when using this Code!
  * @INFO
+ * Fix Almazmsi | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js/issues/100
  */
